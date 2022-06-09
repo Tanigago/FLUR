3993ee1a7a3d41ab34887a906ca8c83978b7fd0f
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-06-09 14:50:18 +0200

    Add db information

---
+++ project/backend/src/index.mjs
@@ -0,0 +1,205 @@
+import { config } from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+
+if (process.env.NODE_ENV !== 'production') config();
+
+import {
+    sqlCallback,
+    findUser,
+    findCollection,
+    findShoes,
+    insertUser,
+    insertCollection,
+    insertShoes,
+    getUsers,
+    getCollection,
+    getShoes
+} from "./db.mjs";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Nuestro modelo de datos.
+class User {
+    constructor (name, email, password, photoProfile) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.photoProfile = photoProfile;
+        this.idUser = Date.now();
+    }
+}
+
+class Collection {
+    constructor (nameCollection, waterproofCollection, warmCollection, seasonCollection) {
+        this.nameCollection = nameCollection;
+        this.waterproofCollection = waterproofCollection;
+        this.warmCollection = warmCollection;
+        this.seasonCollection = seasonCollection;
+        this.idCollection = Date.now();
+    }
+}
+
+class Shoes {
+    constructor (photoProfile) {
+        this.photoProfile = photoProfile;
+        this.idShoes = Date.now();
+    }
+}
+const users = [];
+const collection = [];
+const shoes = [];
+
+function decodeBasicToken(request) {
+    const [ authType, b64token ] = request.headers.authorization.split(" ",2);
+    const token = atob(b64token);
+    return token.split(":",2);
+}
+
+function authMiddleware (request, response, next) {
+    try {
+        if ( ! request.headers.authorization ) {
+            response.status(401);
+            response.send(`Authentication requiered.`);
+            return
+        } else {
+            const [ authType, b64token ] = request.headers.authorization.split(" ",2);
+            if ( authType !== "Basic") {
+                response.status(400);
+                response.send(`Ùnknown authentication type: ${authType}`);
+                return
+            }
+            const [ source, password ] = decodeBasicToken(request);
+            findSource(source, password, (error, data)=>{
+                if (error) {
+                    console.error(error)
+                    throw error;
+                }
+                if ( data ) {
+                    next();
+                } else {
+                    response.status(401);
+                    response.send('Unauthorized');
+                    return
+                }
+            });
+        };
+    } catch (err) {
+        response.status(500)
+        response.send(err)
+        return
+    }
+}
+
+app.post('/login/', (request, response) => {
+    try {
+        const { userName, password } = request.body;
+        if ( ! userName || ! password ) {
+            response.status(400)
+            response.send("Must provide 'userName' and 'password' JSON");
+            return
+        }
+        findUser(userName, password, (error, data)=>{
+            if (error) {
+                console.error(error)
+                throw error;
+            }
+            if ( data ) {
+                response.status(401);
+                response.send("Usuario ya registrado");
+                return
+            } else {
+                const newUser = new User(userName, password);
+                insertUser(newUser,sqlCallback);
+                const json = JSON.stringify(newUser.id)
+                response.send(json);
+                return
+            }
+        });
+    } catch (err) {
+        response.status(500)
+        response.send(err)
+        return
+    }
+});
+
+app.get('/users/', (request, response)=>{
+    try {
+        getUsers((error, data)=>{
+            if ( error ) {
+                console.error(error);
+                response.status(500)
+                response.send("Database error.")
+                return
+            }
+            if ( data ){
+                const json = JSON.stringify(data)
+                response.send(json);
+                return
+            }
+        });
+    } catch (err) {
+        response.status(500)
+        response.send(err)
+        return
+    }
+});
+
+app.post('/message/', authMiddleware, (request, response) => {
+    try {
+        const [ source ] =  decodeBasicToken(request)
+        const { content } = request.body;
+        if ( ! source || ! content ) {
+            response.status(400)
+            response.send("Must provide a valid authentication token and a 'content' JSON");
+            return
+        }
+        const newMessage = new Message(source, content);
+        insertMessage(newMessage);
+        getLastMessages(1, (error, data)=>{
+            if ( error ) {
+                console.error(error);
+                response.status(500)
+                response.send("Database error.")
+                return
+            }
+            if ( data ) {
+                response.json(data);
+                response.send();
+                return
+            }
+        })
+    } catch (err) {
+        response.status(500)
+        response.send(err)
+        return
+    }
+});
+
+app.get('/messages/', authMiddleware, (request, response) => {
+    try {
+        getLastMessages(15, (error, data)=>{
+            if ( error ) {
+                console.error(error);
+                response.status(500)
+                response.send("Database error.")
+                return
+            }
+            if ( data ) {
+                response.json(data);
+                response.send();
+                return
+            }
+        })
+    } catch (err) {
+        response.status(500)
+        response.send(err)
+        return
+    }
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on http://127.0.0.1:${process.env.PORT}`)
+})
--- project/backend/src/models/dbFLUR.mjs
+++ project/backend/src/models/dbFLUR.mjs
@@ -1,7 +1,7 @@
 import sqlite3 from 'sqlite3';
 
 
-export const db = new sqlite3.Database('./FLUR.db', (err)=> {
+export const db = new sqlite3.Database('./FLUR.db', (err) => {
     if (err) {
         console.error(err.flur);
     }
@@ -17,14 +17,14 @@ db.run(`
             name TEXT NOT NULL,
             email TEXT NOT NULL,
             password TEXT NOT NULL,
-            photoProfile TEXT NOT NULL
+            photoProfile TEXT
         )
 `);
 
 db.run(`
     CREATE TABLE
         IF NOT EXISTS
-        collections(
+        collection(
             idCollection INTEGRER PRIMARY KEY AUTO_INCREMENT,
             nameCollection TEXT NOT NULL,
             waterproofCollection TEXT NOT NULL,
@@ -56,4 +56,77 @@ db.run(`
 
 
         )
-`)
+`);
+
+export function sqlCallback(error, data) {
+    console.log("error:", error, "data:", data);
+    if (error) throw error;
+}
+
+export function findUser(name, password, callback) {
+    db.get(`
+        SELECT idUser
+        FROM users
+        WHERE name ="${name}" AND password = "${password}"
+        `,
+        callback
+    )
+}
+
+export function findCollection(idCollection, callback) {
+    db.get(`
+        SELECT *
+        FROM collection
+        WHERE idCollection = "${idCollection}"
+        `,
+        callback
+    )
+}
+
+export function findShoes (idShoes, callback) {
+    db.get(`
+        SELECT *
+        FROM shoes
+        WHERE idShoes = "${idShoes}"
+        `,
+        callback
+    )
+}
+
+export function insertUser ( userObject, callback ) {
+    const { idUser, name, email, password, photoProfile } = userObject;
+    const sql = `
+        INSERT INTO users (idUser, name, email, password)
+        values (${idUser}, "${name}", "${email}", "${password}", "${photoProfile}");
+    `;
+    db.run(sql,callback);
+}
+export function insertCollection ( collectionOBject, callback) {
+    const { idCollection, nameCollection, waterproofCollection, warmCollection, seasonCollection } = collectionOBject;
+    const sql = `
+        INSERT INTO collection (idCollection, nameCollection, waterproofCollection, warmCollection, seasonCollection)
+        values (${idCollection}, "${nameCollection}", "${waterproofCollection}", "${warmCollection}", "${seasonCollection}");
+    `;
+    db.run(sql,callback);
+}
+
+export function insertShoes ( shoesOBject, callback) {
+    const { idShoes, photoShoes } = shoesOBject;
+    const sql = `
+        INSERT INTO shoes (idShoes, photoShoes)
+        values (${idShoes}, "${photoShoes}");
+    `;
+    db.run(sql,callback);
+}
+
+export function getUsers ( callback ) {
+    db.all("SELECT idUser, name FROM users", callback);
+}
+
+export function getCollection ( callback ) {
+    db.all("SELECT idCollection, name FROM collection", callback);
+}
+
+export function getShoes ( callback ) {
+    db.all("SELECT idShoes, name FROM shoes", callback);
+}
