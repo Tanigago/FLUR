3865e507b9f69153ce2db430c430aa45b3f4afe9
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-07-04 11:04:21 +0200

    Reorganized all files

---
+++ frontend/src/services/SharedStorage.jsx
@@ -0,0 +1,52 @@
+import { useState, createContext } from "react";
+
+export const Context = createContext();
+
+export function ContextProvider({ children }) {
+
+    const [colecciones, setColecciones] = useState([
+        {
+            tagtext: "",
+            tagpath: "",
+            count: 0,
+            pluralize: true,
+            tagdelbutton: true,
+        }
+    ]);
+
+    const [calzados, setCalzados] = useState([
+        {
+            iditem: "",
+            imgUrl: "",
+        }
+    ]);
+
+    const [userData, setUserData] = useState([
+        {
+            name: "",
+            email: "",
+            password: ""
+        }
+    ]);
+
+    const storage = {
+        states: {
+            colecciones,
+            calzados,
+            userData
+        },
+        actions: {
+            updateUserData: function (userDataIdx, newUserData) {
+                const oldUserData = [...userData]
+                oldUserData[userDataIdx] = newUserData
+                setUserData([...oldUserData])
+            }
+        }
+    };
+
+    return (
+        <Context.Provider value={storage}>
+            {children}
+        </Context.Provider>
+    );
+}
---
+++ frontend/src/tools/controllers.mjs
@@ -0,0 +1,61 @@
+import { HOST } from "./defines.mjs"
+
+function dataToString(dataAtTrasf) {
+    const jsonData = JSON.stringify(dataAtTrasf);
+    const params = {
+        method: 'POST',
+        body: jsonData,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    };
+    return params;
+}
+
+//FETCH POST PARA EL REGISTRO
+
+export async function postUser(data) {
+    const params = dataToString(data);
+    const response = await fetch(
+        HOST + '/api/v0.0/user/',
+        params
+    );
+    return await response.json();
+}
+
+//FETCH GET PARA OBTENER USUARIO
+export async function getUsers(HOST) {
+    const response = await fetch(HOST + '/api/v0.0/users/');
+    const data = await response.json();
+    return data;
+}
+
+//FETCH POST PARA EL LOGIN
+export async function logingUser(data) {
+    const params = dataToString(data);
+    const response = await fetch(
+        HOST + '/api/v0.0/login/',
+        params
+    );
+    return await response.json();
+}
+
+//FETCH POST PARA NUEVA COLECCIÓN
+export async function getNewCollection(nombre, warm, waterproof, season) {
+    const response = await fetch(
+        HOST + '/api/v0.0/collection/',
+         {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(
+            {
+                nameCollection: nombre,
+                warmlvl: warm,
+                waterprooflvl: waterproof,
+                season: season
+            })
+        })
+    return await response.json()
+};
